26aa8bd14c81b94d20f018d055e92f8aa20ac8a5
tonalidae <eccelyr@unal.edu.co>
2022-01-21 05:34:21 -0500

    feat(Drawer Navigation): Added functionality of  drawer nav

--- navigation/InitialNav.js
+++ navigation/InitialNav.js
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from '@react-navigation/drawer'
+// import { createStackNavigator } from "@react-navigation/stack";
 import SplashScreen from "../screens/SplashScreen";
 import MainScreen from "../screens/MainScreen";
 import LoginScreen from "../screens/LoginScreen";
@@ -9,30 +10,52 @@ import PersonalFinanceScreen from "../screens/PersonalFinanceScreen";
 import MonthlyCustomersScreen from "../screens/MonthlyCustomersScreen";
 import AccountScreen from "../screens/AccountScreen";
 
-const Stack = createStackNavigator();
+
+
+const Drawer = createDrawerNavigator();
+// const Stack = createStackNavigator();
 const InitialNav = () => {
     return (
-        <Stack.Navigator
-            screenOptions={{
-                headerShown: false,
-            }}
-            initialRouteName="Account"
-        >
-            <Stack.Screen
-                name="PersonalFinance"
-                component={PersonalFinanceScreen}
-            />
-            <Stack.Screen
-                name="MonthlyCustomers"
-                component={MonthlyCustomersScreen}
-            />
-            <Stack.Screen name="Account" component={AccountScreen} />
-            <Stack.Screen name="CostCenter" component={CostCenterScreen} />
-            <Stack.Screen name="General" component={GeneralScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Splash" component={SplashScreen} />
-            <Stack.Screen name="Main" component={MainScreen} />
-        </Stack.Navigator>
+        <Drawer.Navigator initialRouteName="General">
+            <Drawer.Screen name="General" component={GeneralScreen}/
+            >
+                <Drawer.Screen name="PersonalFinance" component={PersonalFinanceScreen}/>
+                <Drawer.Screen name="MonthlyCustomers" component={MonthlyCustomersScreen }/>
+
+
+
+
+        </Drawer.Navigator>
+
+
+
+
+
+
+
+
+
+        // <Stack.Navigator
+        //     screenOptions={{
+        //         headerShown: false,
+        //     }}
+        //     initialRouteName="Account"
+        // >
+        //     <Stack.Screen
+        //         name="PersonalFinance"
+        //         component={PersonalFinanceScreen}
+        //     />
+        //     <Stack.Screen
+        //         name="MonthlyCustomers"
+        //         component={MonthlyCustomersScreen}
+        //     />
+        //     <Stack.Screen name="Account" component={AccountScreen} />
+        //     <Stack.Screen name="CostCenter" component={CostCenterScreen} />
+        //     <Stack.Screen name="General" component={GeneralScreen} />
+        //     <Stack.Screen name="Login" component={LoginScreen} />
+        //     <Stack.Screen name="Splash" component={SplashScreen} />
+        //     <Stack.Screen name="Main" component={MainScreen} />
+        // </Stack.Navigator>
     );
 };
 
